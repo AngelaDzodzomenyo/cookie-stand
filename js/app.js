f67cf1e14103db9c958d04402e2e518213d2a054
Angela Dzodzomenyo <enyodzo@gmail.com>
2021-08-17 21:49:36 -0500

    update files

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
+'use strict';
+
+console.log('Hello!');
+
+const locationsDiv = document.getElementById('locations');
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm']
 
 
-const Seattle = {
+const seattle = {
   location: 'Seattle',
   minCust: 23,
   maxCust: 65,
@@ -18,33 +23,16 @@ const Seattle = {
     hourlyCookieTotal = Math.round(hourlyCookieTotal)
     console.log(hourlyCookieTotal)
   },
-  salePerHour: [],
+  salePerHourArray: [],
   currentSalePerHour: function(){
    for(let i=0; i < hours.length; i++){
-    this.salePerHour.push(this.dailySales());
+    this.salePerHourArray.push(this.dailySales());
   }
-},
-
-displaySales: function(salesArray) {
-  //This goes into an article? I think
-  const articleElem = documenbt.createElement('article');
-  const h1Elem = document.createElement('h1')
-  salesDiv.appendChild(h1Elem)
-  h1Elem.textContent - this.location
-  const olElem = document.createeElement('ol');
-  salesDiv.appendChild(articleElem)
-  articlesElem.appendChild(olElem)
-  olElem.setAttribute("start", "6")
-
-  for (let i =0; i < SeattleStore.salePerHour.length; i++) {
-    
-  }
-  const liElem
+}
 }
 
-Seattle.dailySales(),
 
-const Tokyo = {
+const tokyo = {
   location: 'Tokyo',
   minCust: 3,
   maxCust: 24,
@@ -59,17 +47,19 @@ const Tokyo = {
     let hourlyCookieTotal = randomCust*this.avgCookieSale
     hourlyCookieTotal = Math.round(hourlyCookieTotal)
     console.log(hourlyCookieTotal)
-  }
-  salePerHour: [],
+  },
+  salePerHourArray: [],
   currentSalePerHour: function(){
    for(let i=0; i < hours.length; i++){
-    this.salesPerHour.push(this.dailySales());
+    this.salePerHourArray.push(this.dailySales());
   }
-},
 }
-Tokyo.dailySales()
+}
 
-const Dubai = {
+
+
+
+const dubai = {
   location: 'Dubai',
   minCust: 11,
   maxCust: 38,
@@ -84,17 +74,19 @@ const Dubai = {
     let hourlyCookieTotal = randomCust*this.avgCookieSale
     hourlyCookieTotal = Math.round(hourlyCookieTotal)
     console.log(hourlyCookieTotal)
-  }
-  salePerHour: [],
+  },
+  salePerHourArray: [],
   currentSalePerHour: function(){
    for(let i=0; i < hours.length; i++){
-    this.salesPerHour.push(this.dailySales());
+    this.salePerHourArray.push(this.dailySales());
   }
 },
 }
-Dubai.dailySales()
 
-const Paris = {
+
+
+
+const paris = {
   location: 'Paris',
   minCust: 20,
   maxCust: 38,
@@ -109,17 +101,19 @@ const Paris = {
     let hourlyCookieTotal = randomCust*this.avgCookieSale
     hourlyCookieTotal = Math.round(hourlyCookieTotal)
     console.log(hourlyCookieTotal)
-  }
-  salePerHour: [],
+  },
+  salePerHourArray: [],
   currentSalePerHour: function(){
    for(let i=0; i < hours.length; i++){
-    this.salesPerHour.push(this.dailySales());
+    this.salePerHourArray.push(this.dailySales());
   }
 },
 }
-Paris.dailySales()
 
-const Lima = {
+
+
+
+const lima = {
   location:'Lima',
   minCust: 2,
   maxCust: 16,
@@ -134,25 +128,59 @@ const Lima = {
     let hourlyCookieTotal = randomCust*this.avgCookieSale
     hourlyCookieTotal = Math.round(hourlyCookieTotal)
     console.log(hourlyCookieTotal)
-  }
-  salePerHour: [],
+  },
+  salePerHourArray: [],
   currentSalePerHour: function(){
    for(let i=0; i < hours.length; i++){
-    this.salesPerHour.push(this.dailySales());
+    this.salePerHourArray.push(this.dailySales());
   }
 },
 }
-Lima.dailySales()
+
+function _makeElement(tag, parent, text) {
+  const element = document.createElement(tag);
+  parent.appendChild(element);
+  if (text) {
+    element.textContent = text;
+  }
+  return element;
+}
+
+//location 
+function renderLocation(location) {
+  let total = 0;
+  const articleElem = _makeElement('article', locationsDiv, null);
+  _makeElement('h3', articleElem, location.name);
+  const ulElem = _makeElement('ul', articleElem, null)
+  for (let i = 0; i < hours.length; i++) {
+    const text = `${hours[i]}: ${location.salePerHourArray[i]} cookies`;
+    total += location.salePerHourArray[i];
+    _makeElement('li', ulElem, text);
+  }
+  let totalsString = `Total: ${total} cookies`;
+  _makeElement('li', ulElem, totalsString);
+}
+
+
+const locationsArray = [seattle, tokyo, dubai, paris, lima]
+
+function renderAllLocations() {
+  for (let i = 0; i < locationsArray.length; i++) {
+    const currentLocation = locationsArray[i]
+    currentLocation.currentSalePerHour()
+    renderLocation(currentLocation);
+  }
+}
+
+renderAllLocations();
 
 
 
 
-
-
-
-
-
-
+//open <article>
+//location as a title on <h3>
+//i want <ul> and <li> to hold time 
+//then close </article>
 
 
 
